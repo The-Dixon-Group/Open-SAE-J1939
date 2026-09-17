a5171ba9be54717f8edaec3b378797808950537b
swalia001482 <130368744+swalia001482@users.noreply.github.com>
2026-09-17 17:17:34 -0400

    <feature> SAE_J1939_Send_Product_List added to Request_Proprietary_B.c
    SAE_J1939_Send_Product_List now called in Request_Proprietary_B.c and checks if the correct compartment was requested, if so it will send over the information
    
    Signed-off-by: swalia001482 <130368744+swalia001482@users.noreply.github.com>

--- Src/SAE_J1939/SAE_J1939-71_Application_Layer/Request_Proprietary_B.c
+++ Src/SAE_J1939/SAE_J1939-71_Application_Layer/Request_Proprietary_B.c
@@ -116,16 +116,17 @@ void SAE_J1939_Read_Response_Request_Proprietary_B(J1939* j1939, uint8_t SA, uin
             }
             break;
         }
-        /*
+        
         case PRODUCT_LIST_PGN:
         {
             uint8_t broadcastcompartmentPosition = data[0];
             if (broadcastcompartmentPosition == compartmentPosition){
                 //Send Over Product List if Compartment Matches
+                SAE_J1939_Send_Product_List(j1939,0xFF);
             }
             break;
         }
-         * */
+        
         /*
          * Add more PGNs here as needed.
          *
